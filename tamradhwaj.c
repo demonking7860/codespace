2b7f53f05a073365b98d14bf3296ed683072d982
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-03 18:41:40 +0000

    Mon, Apr 3, 2023, 6:41 PM +00:00

---
+++ tamradhwaj.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int a,b,sum;
+  printf("enter the number a");
+  scanf("%d",&a);
+   printf("enter the number b");
+  scanf("%d",&b);
+  sum=a+b;
+  printf("the sum of a and b is %d",sum);
+
+
+
+
+
+
+    return 0;
+}
