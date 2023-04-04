50b62bc2f2c707a6330b810ac21165247d3e0df3
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-04 09:09:01 +0000

    Tue, Apr 4, 2023, 9:09 AM +00:00

---
+++ hello2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void)
+{
+    string sexy = get_string("give me ?");
+    string hot  = get_string("hand ?");
+    printf("i want to give u , %s%s\n" , sexy , hot);
+}
