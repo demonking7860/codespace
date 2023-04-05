8fb7bdb53c5ec8a134902046f64f37ec8fa0fe84
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-05 05:25:19 +0000

    Wed, Apr 5, 2023, 5:25 AM +00:00

--- lab.c
+++ lab.c
@@ -6,16 +6,17 @@ int main (void)
 {
 
     int s ;
-    
+    if(s>9){
 
     do
     {
         s = get_int("get size : ");
     }
     while (" s < 9 ");
-
+    }
+    else{
 
     int e = get_int("ending size : ");
 
-
+    }
 }
