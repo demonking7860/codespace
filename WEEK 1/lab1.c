fe35779cb5522fc084a88304bcb1a4584d1149b3
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-05 04:50:19 +0000

    Wed, Apr 5, 2023, 4:50 AM +00:00

--- WEEK 1/lab1.c
+++ WEEK 1/lab1.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
+
 #include<cs50.h>
 
 int main (void)
 {
     int s = get_int("starting size");
 
-    int e = get_int("ending size");
-
-    do
+     do
     {
         s = get_int("staring size");
     }
     while
     ("s < 9");
+    
+    int e = get_int("ending size");
+
+
 
 
 }
