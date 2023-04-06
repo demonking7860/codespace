28f9aa40e095c1e99903e15f62067ea89f8102a0
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-06 12:46:58 +0000

    Thu, Apr 6, 2023, 12:46 PM +00:00

--- marioo.c
+++ marioo.c
@@ -12,4 +12,12 @@ int main(void)
     }
     while( h > 8 ||  h < 1 );
 
+    for (int i = 1 ; i<=h;i++)
+    {
+        for( int j = 1 ; j<=i ; j++)
+        printf("# ");
+
+    }
+    printf("\n");
+
 }
