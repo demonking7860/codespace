3cc8a1d46f6c47737fdca027d987a52623ddd0fc
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-06 11:48:35 +0000

    Thu, Apr 6, 2023, 11:48 AM +00:00

--- marioo.c
+++ marioo.c
@@ -6,8 +6,12 @@ int main(void)
     // prompt the user for input
     int h = get_int("height: ");
 
-    while( 1 < h , h<8 )
+    while( 8 <= h  )
     {
         h = get_int("height: ");
+    }
+    while(h<=1)
+    {
+        h = get_int("height");
     }
 }
