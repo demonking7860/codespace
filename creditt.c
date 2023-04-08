eae7e91820ec787f29e32c9d94f283952b088300
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-08 17:28:49 +0000

    Sat, Apr 8, 2023, 5:28 PM +00:00

---
+++ creditt.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    long apex ;
+    apex = get_long_long("Number : ");
+
+    if(apex < (34*10^13) ||   apex > 359999999999999 )
+    {
+        printf("INVALID\n");
+    }
+    else
+    {
+        printf("APEX CARD\n");
+    }
+}
