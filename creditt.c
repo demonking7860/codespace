759d1de0846a2e4015b7b0bea44c413e1d7eeb02
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-09 10:21:06 +0000

    Sun, Apr 9, 2023, 10:21 AM +00:00

--- creditt.c
+++ creditt.c
@@ -6,7 +6,7 @@ int main(void)
     long apex ;
     apex = get_long("Number : ");
 
-    if(( apex%1000000000000 ) ==( 34 | 35) )
+    if(( apex%1000000000000 ) !=( 34 | 35) )
     {
         printf("APEX CARD\n");
     }
