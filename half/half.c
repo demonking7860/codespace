1fc743854904feee986163fe93e02278a7e95c25
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-09 13:34:43 +0000

    Sun, Apr 9, 2023, 1:34 PM +00:00

--- half/half.c
+++ half/half.c
@@ -12,7 +12,7 @@ int main(void)
     float tax_percent = get_float("Sale Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
 
-    
+    Sale Tax Percent = 
 
     printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
 }
