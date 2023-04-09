4fa713b043c0297886c69f4f70ff33cc8aa9cda6
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-04-09 13:01:06 +0000

    Sun, Apr 9, 2023, 1:01 PM +00:00

---
+++ debug.c
@@ -0,0 +1,11 @@
+#include<cs50.h>
+
+#include<stdio.h>
+
+int main (void)
+{
+    string first = get_string(" What is your name? ");
+    string last = get_string(" Where do you live? ");
+    printf("hello %s %s\n",  first , from last);
+
+}
