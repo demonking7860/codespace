5753a69e14a895f225eaf6cfaad0668c7c72ab83
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-05-21 20:14:24 +0000

    Sun, May 21, 2023, 8:14 PM +00:00

--- scores.c
+++ scores.c
@@ -15,7 +15,7 @@ string player1 = get_string("word: ");
         n++;
     }
     printf("%i\n", n);
-    for(int m=0, m<=n,m++)
+    for(int m=0, m<n,m++)
     {
         sum=0;
         sum=sum + points[m] ;
