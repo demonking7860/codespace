cf6436891716703c36d29bedb1e81f69733fd237
demonking7860 <124158966+demonking7860@users.noreply.github.com>
2023-05-21 20:07:57 +0000

    Sun, May 21, 2023, 8:07 PM +00:00

--- scores.c
+++ scores.c
@@ -7,12 +7,13 @@ int main (void)
 int a=1,b=3,c=3,d=2;
 char points[]={a,b,c,d};
 
-string player1 = get_string("word ");
+string player1 = get_string("word: ");
 
  int n = 0;
-    while (word[n] != '\0')
+    while (player1[n] != '\0')
     {
         n++;
     }
-    printf("%i\n", n)
+    printf("%i\n", n);
+    
 }
